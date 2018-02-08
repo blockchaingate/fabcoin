165363d5d51858fb2775b4207f2c6ac16c6c9113
Sam Gong <sam@fa.biz>
2018-02-08 10:29:25 -0500

    revert from  SmartContract_Opcodes

--- src/script/script.cpp
+++ src/script/script.cpp
@@ -139,11 +139,6 @@ const char* GetOpName(opcodetype opcode)
     case OP_NOP9                   : return "OP_NOP9";
     case OP_NOP10                  : return "OP_NOP10";
 
-    // Smart contract VM extension
-    case OP_CREATE                 : return "OP_CREATE";
-    case OP_CALL                   : return "OP_CALL";
-    case OP_SPEND                  : return "OP_SPEND";
-
     case OP_INVALIDOPCODE          : return "OP_INVALIDOPCODE";
 
     // Note:
