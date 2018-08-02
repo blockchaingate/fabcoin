c8c6322796dfd758df6edc87a330855d95c48c9d
jonathanyan <jontera@gmail.com>
2018-08-02 15:01:30 -0400

    fix reindex issue

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -396,6 +396,9 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
                 pindexNew->nSolution      = diskindex.nSolution;
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
+                pindexNew->hashStateRoot  = diskindex.hashStateRoot; // fasc
+                pindexNew->hashUTXORoot   = diskindex.hashUTXORoot; // fasc
+
 
                 // TODO(h4x3rotab): Check Equihash solution? Not sure why Zcash doesn't do it here.
                 bool postfork = pindexNew->nHeight >= consensusParams.FABHeight;
