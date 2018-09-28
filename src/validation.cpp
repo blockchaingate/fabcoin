756c719416a798e50a50d2ff1cd5a53f04adf307
xianlingong <xianlingong@msn.com>
2018-09-28 14:49:24 -0400

    Fix crash bug with duplicate inputs within a transaction

--- src/validation.cpp
+++ src/validation.cpp
@@ -3813,7 +3813,8 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
     bool lastWasContract=false;
     // Check transactions
     for (const auto& tx : block.vtx) {
-        if (!CheckTransaction(*tx, state, false))
+        //if (!CheckTransaction(*tx, state, false))
+        if (!CheckTransaction(*tx, state, true))
             return state.Invalid(false, state.GetRejectCode(), state.GetRejectReason(),
                                  strprintf("Transaction check failed (tx hash %s) %s", tx->GetHash().ToString(), state.GetDebugMessage()));
         //OP_SPEND can only exist immediately after a contract tx in a block, or after another OP_SPEND
