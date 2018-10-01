8e965d0aa088d7eb62c78b487e330e0a9fec5265
xianlingong <xianlingong@msn.com>
2018-10-01 10:39:12 -0400

    merge to v16 bench/zmq

--- src/bench/bench_fabcoin.cpp
+++ src/bench/bench_fabcoin.cpp
@@ -18,7 +18,7 @@ static const int64_t DEFAULT_BENCH_EVALUATIONS = 5;
 static const char* DEFAULT_BENCH_FILTER = ".*";
 static const char* DEFAULT_BENCH_SCALING = "1.0";
 static const char* DEFAULT_BENCH_PRINTER = "console";
-static const char* DEFAULT_PLOT_PLOTLYURL = "http://cdn.plot.ly/plotly-latest.min.js";
+static const char* DEFAULT_PLOT_PLOTLYURL = "https://cdn.plot.ly/plotly-latest.min.js";
 static const int64_t DEFAULT_PLOT_WIDTH = 1024;
 static const int64_t DEFAULT_PLOT_HEIGHT = 768;
 
