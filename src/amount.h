db375300cd7e3b9abe43d8d2d465cb6d5acce3a9
jason <hzx1897@hotmail.com>
2019-11-04 14:02:30 -0500

    change MAX_MONEY to 74 millions

--- src/amount.h
+++ src/amount.h
@@ -20,7 +20,7 @@ static const CAmount CENT = 1000000;
 extern const std::string CURRENCY_UNIT;
 
 /** No amount larger than this (in liu) is valid.*/
-static const CAmount MAX_MONEY = (168000000 + 32000000) * COIN;
+static const CAmount MAX_MONEY = (42000000 + 32000000) * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif //  FABCOIN_AMOUNT_H
